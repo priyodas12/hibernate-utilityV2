c243c96d8d2cb2536df6feecb526371c8ea1d848
Priyobrato Das <priyodas12@gmail.com>
2020-08-08 22:53:39 +0530

    shutdown behavior

--- src/main/java/utility/HibernateUtility.java
+++ src/main/java/utility/HibernateUtility.java
@@ -6,6 +6,12 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+/*
+* ServiceRegistry: The central service API, aside from the services themselves, is the org.hibernate.service.ServiceRegistry interface. The main purpose of a service registry is to hold, manage and provide access to services.
+* MetadataSources: Entry point into working with sources of metadata information (mapping XML, annotations). Tell Hibernate about sources and then call buildMetadata(), or use getMetadataBuilder() to customize how sources are processed (naming strategies, etc).
+*
+*/
+
 public class HibernateUtility {
     private static StandardServiceRegistry standardServiceRegistry;
     private static SessionFactory sessionFactory;
@@ -25,6 +31,7 @@ public class HibernateUtility {
 
                 //get session
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
+
             } catch (Exception e) {
                 e.printStackTrace();
                 // terminate standardServiceRegistry instance for exception
@@ -34,10 +41,12 @@ public class HibernateUtility {
             }
         }
     }
+
     // single end point for getting session
     public static SessionFactory getSessionFactory(){
         return sessionFactory;
     }
+
     // terminate standardServiceRegistry to close all Session factory instance.
     public static void shutdown() {
         System.out.println("Shutting Down");
